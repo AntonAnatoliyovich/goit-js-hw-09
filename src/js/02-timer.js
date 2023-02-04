c3cab00e296fb35cca7f742ba009be7c95f1e0ba
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-02-04 15:20:28 +0200

    init

---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+
+
+
+const input = document.getElementById("datetime-picker");
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const Btn = document.querySelector('[data-start]');
+const countDownDate = new Date();
+
+const now = Date.now();
+console.log(now);
+
+const featureDate = input;
+console.log(featureDate);
