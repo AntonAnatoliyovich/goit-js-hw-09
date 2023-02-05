2ea12204497571dd96a9f09b388808e758a9e14e
AntonAnatoliyovich <113045051+AntonAnatoliyovich@users.noreply.github.com>
2023-02-05 11:23:34 +0000

    Deploying to gh-pages from @ AntonAnatoliyovich/goit-js-hw-09@c423b26f770db02294470129f9327ba5aded67cb 🚀

---
+++ 01-color-switcher.bd5e8857.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let n=null;function o(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){n=setInterval(o,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.bd5e8857.js.map
